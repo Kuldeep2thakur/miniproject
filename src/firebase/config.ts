03714eb18885c73a3d286865307bfc69fba7c59a
Kuldeep Sengar <kuldeepsengar5678@gmail.com>
2025-10-02 17:34:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-782762306-71927",
+  "appId": "1:1032186678329:web:274bdd06dd73eeda9811e8",
+  "apiKey": "AIzaSyDJ0BgOxSvQc_EyMCVoBKZqqtTnYBgGLoI",
+  "authDomain": "studio-782762306-71927.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1032186678329"
+};
